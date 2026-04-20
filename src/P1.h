5b4a5886c9440ab2ff9e9988ab36aa7c05131e90
Gabriel Murillo <gabrielmurillomachicao@gmail.com>
2026-04-20 09:01:03 -0500

    Se ha implementado la clase contadar char que cuenta las veces que una letra aparece en una lista o string, todavía no se han encontrado erroes, pero falta mandar al autograder

--- src/P1.h
+++ src/P1.h
@@ -4,8 +4,54 @@
 
 #ifndef PROG3_UNIT2_TEMPLATE_FUNCTION_V2023_1_P1_H
 #define PROG3_UNIT2_TEMPLATE_FUNCTION_V2023_1_P1_H
-
+#include <deque>
 
 void question_1();
+namespace utec {
+    class count_char {
+        int count;
+        public:
+        count_char()=default;
+        count_char(const std::string& texto, const char objetivo) {
+            int contador=0;
+            std::string aux(1,objetivo);
+            for ( int i=0; i<texto.length(); i++) {
+                std::string a = texto.substr(i,1);
+                if ( a == aux) {
+                    contador++;
+                }
 
+            }
+            count = contador;
+        };
+        template<typename T, std::size_t N>
+        count_char(const std::array<T,N> arr, const char& objetivo) {
+            int contador=0;
+            for ( int i=0; i<arr.size(); i++) {
+                char a = arr.at(i);
+                if ( a == objetivo) {
+                    contador++;
+                }
+            }
+            count = contador;
+        }
+        template<typename T>
+        count_char(const std::deque<T> text,const char& objetivo) {
+            int contador=0;
+            for ( int i=0; i<text.size(); i++) {
+                char a=text.at(i);
+                if ( a == objetivo) {
+                    contador++;
+                }
+            }
+            count = contador;
+        }
+        friend std::ostream& operator<<(std::ostream& os,const count_char Objetivo) {
+            os << Objetivo.count;
+            return os;
+
+        }
+        ~count_char()=default;
+    };
+}
 #endif //PROG3_UNIT2_TEMPLATE_FUNCTION_V2023_1_P1_H
