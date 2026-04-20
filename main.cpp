6244b4878dd651decea60576aee9a6f0f8c2fb77
Gabriel Murillo <gabrielmurillomachicao@gmail.com>
2026-04-20 12:51:15 -0500

    Se implemento la función split que divide un string por una señal que elija el usuario

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
-
+#include <vector>
+std::vector<std::string> split(std::string a, char b) {
+    std::vector<std::string> v;
+    int contador=0;
+    int contador2=0;
+    for (int i=0; i < a.size(); i++) {
+        contador2++;
+        if (a[i] == b) {
+            v.push_back(a.substr(contador,contador2));
+            contador+=contador2;
+            contador2=0;
+        }
+    }
+    return v;
+};
 int main() {
-    std::cout << "Hola Mundo" << std::endl;
+    auto result = split(
+      "Universidad de Ingenieria y Tecnologia",
+      ' ');
+    std::cout << result.size() << std::endl;
     return 0;
 }
--- src/P2.h
+++ src/P2.h
@@ -4,8 +4,38 @@
 
 #ifndef PROG3_UNIT2_TEMPLATE_FUNCTION_V2023_1_P2_H
 #define PROG3_UNIT2_TEMPLATE_FUNCTION_V2023_1_P2_H
-
-
+#include <vector>
+#include<string>
 void question_2();
+namespace utec {
+    inline std::vector<std::string> split(std::string a, char b) {
+        std::vector<std::string> v;
+        int contador=0;
+        int contador2=0;
+        for (int i=0; i < a.size(); i++) {
+            contador2++;
+            if (a[i] == b||i==a.size()-1) {
+                v.push_back(a.substr(contador,contador2));
+                contador+=contador2;
+                contador2=0;
+            }
+        }
+        return v;
+    };
+    inline std::vector<std::wstring> split(std::wstring a, wchar_t b) {
+        std::vector<std::wstring> v;
+        int contador=0;
+        int contador2=0;
+        for (int i=0; i < a.size(); i++) {
+            contador2++;
+            if (a[i] == b||i==a.size()-1) {
+                v.push_back(a.substr(contador,contador2));
+                contador+=contador2;
+                contador2=0;
+            }
+        }
+        return v;
+    };
 
+}
 #endif //PROG3_UNIT2_TEMPLATE_FUNCTION_V2023_1_P2_H
